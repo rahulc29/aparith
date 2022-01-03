d25af3e9166759a55f90e09f3e3336ba6cb2d1a7
rahulc29 <rahul29112002@gmail.com>
2022-01-03 15:01:43 +0530

    Implement unsigned integer addition with vectors

--- src/big_int.rs
+++ src/big_int.rs
@@ -7,9 +7,8 @@ pub enum Sign {
     Zero,
 }
 
-#[derive(Copy)]
 pub struct BigInt {
-    magnitude: Vec<u32>,
+    pub magnitude: Vec<u32>,
     sign: Sign,
 }
 
@@ -46,40 +45,41 @@ impl Add<BigInt> for BigInt {
     type Output = BigInt;
 
     fn add(self, rhs: BigInt) -> Self::Output {
-        if Sign::Zero = self.sign {
-            rhs
-        }
-        if Sign::Zero = rhs.sign {
-            self.clone()
-        }
-
         todo!()
     }
 }
 
-fn add_unsigned(lhs: &BigInt, rhs: &BigInt) -> Vec<u32> {
-    let (smaller, larger) = if lhs.magnitude.len() > rhs.magnitude.len() {
-        (&rhs.magnitude, &lhs.magnitude)
+pub fn add_unsigned(lhs: &Vec<u32>, rhs: &Vec<u32>) -> Vec<u32> {
+    let (smaller, larger) = if lhs.len() > rhs.len() {
+        (rhs, lhs)
     } else {
-        (&lhs.magnitude, &rhs.magnitude)
+        (lhs, rhs)
     };
-    let n = smaller.data.len();
-    let m = larger.data.len();
+    let n = smaller.len();
+    let m = larger.len();
     let mut carry = 0u32;
     let mut to_return = vec![0u32; m];
     for i in 0..n {
-        let (result, overflow) = smaller[i].overflowing_add(larger[i]);
-        to_return[i] = result + carry;
-        if overflow {
-            carry += 1;
-        }
+        let (result, pri_overflow) = smaller[i].overflowing_add(larger[i]);
+        let (result, sec_overflow) = result.overflowing_add(carry);
+        to_return[i] = result;
+        carry = match (pri_overflow, sec_overflow) {
+            (true, true) => 2,
+            (true, false) | (false, true) => 1,
+            (false, false) => 0
+        };
     }
     for i in n..m {
         let (result, overflow) = carry.overflowing_add(larger[i]);
-        to_return[i] = result + carry;
+        to_return[i] = result;
         if overflow {
-            carry += 1;
+            carry = 1;
+        } else {
+            carry = 0;
         }
     }
-    todo!()
+    if carry != 0 {
+        to_return.push(carry);
+    }
+    to_return
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,30 @@ mod big_int;
 
 #[cfg(test)]
 mod tests {
+    use crate::big_int;
+    use crate::big_int::{add_unsigned, BigInt};
+    use crate::big_int::Sign::Positive;
 
+    #[test]
+    fn digit_append_test() {
+        let a = vec![!0u32; 5];
+        let b = vec![1u32];
+        let sum = add_unsigned(&a, &b);
+        assert_eq!(*sum.last().unwrap(), 1);
+    }
+
+    #[test]
+    fn same_length_addition() {
+        let a: Vec<u32> = vec![2, 3, 4];
+        let b: Vec<u32> = vec![5, 6, 6];
+        let sum = add_unsigned(&a, &b);
+        assert_eq!(sum, vec![7, 9, 10]);
+    }
+
+    #[test]
+    fn diff_length_addition() {
+        let a: Vec<u32> = vec![3, 0, 3];
+        let b: Vec<u32> = vec![3, 0, 3, 5];
+        assert_eq!(add_unsigned(&a, &b), vec![6, 0, 6, 5]);
+    }
 }
